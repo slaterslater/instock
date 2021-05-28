35d79d49e5e7de895a899ab1a561f89394b9d5d3
slaterslater <slaterslater@gmail.com>
2021-05-28 14:38:30 -0400

    added logs to see if script executes correctly

--- netlify/functions/instock.js
+++ netlify/functions/instock.js
@@ -9,17 +9,20 @@ async function lookup(){
 
 async function sendMail(message){
   const { MAIL_DOMAIN, MAIL_API_KEY, MAIL_TO, MAIL_FROM} = process.env;
+  console.log({ MAIL_DOMAIN, MAIL_API_KEY, MAIL_TO, MAIL_FROM})
   const mail = mailgun({apiKey: MAIL_API_KEY, domain: MAIL_DOMAIN});
   const data = {
     from: MAIL_FROM,
     to: MAIL_TO,
-    subject: 'V.0.3',
+    subject: 'V.0.4',
     text: message
   };
+  console.log('about to mail...')
   await mail.messages().send(data, (error, body) => {
     if (error) return console.log('Error:', error.message)
     console.log('Success!', body.message);
   });
+  console.log('mail should have sent')
 }
 
 exports.handler = async (event, context) => {
@@ -28,7 +31,7 @@ exports.handler = async (event, context) => {
   // const purchasable = data.availabilities[0].shipping.purchasable
   
   await sendMail('testing netflify')
-  console.log('mail sent and returning status 200')
+  // console.log('mail sent and returning status 200')
   return {
     statusCode: 200,
     body: JSON.stringify(data)
