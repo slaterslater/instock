59fdc01adda594a5aaf55c7199d8a4e75f471015
slaterslater <slaterslater@gmail.com>
2021-05-28 17:20:17 -0400

    remove comments and async form sendMail()

--- netlify/functions/instock.js
+++ netlify/functions/instock.js
@@ -1,5 +1,4 @@
 const axios = require('axios'); 
-// const mailgun = require('mailgun-js');
 
 async function lookup(){
   const { BB_ENDPOINT, PS5 } = process.env;
@@ -7,9 +6,8 @@ async function lookup(){
   return res.data
 }
 
-async function sendMail(message){
+function sendMail(message){
   const { MAIL_DOMAIN, MAIL_API_KEY, MAIL_TO, MAIL_FROM} = process.env;
-  console.log({ MAIL_DOMAIN, MAIL_API_KEY, MAIL_TO, MAIL_FROM})
   const mailgun = require('mailgun-js')({apiKey: MAIL_API_KEY, domain: MAIL_DOMAIN});
   const data = {
     from: MAIL_FROM,
@@ -17,12 +15,9 @@ async function sendMail(message){
     subject: 'V.0.6',
     text: message
   };
-  console.log('about to mail...')
   mailgun.messages().send(data, (error, body) => {
-    console.log('sending...', body)
     if (error) console.log('Error:', error)
   });
-  console.log('...mail should have sent')
 }
 
 exports.handler = async (event, context) => {
@@ -30,8 +25,7 @@ exports.handler = async (event, context) => {
   const data = await lookup()
   // const purchasable = data.availabilities[0].shipping.purchasable
   
-  await sendMail('testing netflify & mailgun')
-  // console.log('mail sent and returning status 200')
+  sendMail('testing netflify & mailgun')
   return {
     statusCode: 200,
     body: JSON.stringify(data)
