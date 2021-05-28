4467fae559ac6f5cc9dac238f09c8af6df15e397
slaterslater <slaterslater@gmail.com>
2021-05-28 16:26:01 -0400

    testing mailgun send function

--- netlify/functions/instock.js
+++ netlify/functions/instock.js
@@ -1,5 +1,5 @@
-const axios = require('axios');
-const mailgun = require("mailgun-js");
+const axios = require('axios'); 
+const mailgun = require('mailgun-js');
 
 async function lookup(){
   const { BB_ENDPOINT, PS5 } = process.env;
@@ -14,15 +14,15 @@ async function sendMail(message){
   const data = {
     from: MAIL_FROM,
     to: MAIL_TO,
-    subject: 'V.0.4',
+    subject: 'V.0.5',
     text: message
   };
   console.log('about to mail...')
   await mail.messages().send(data, (error, body) => {
-    if (error) return console.log('Error:', error.message)
-    console.log('Success!', body.message);
+    console.log('sending...')
+    if (error) console.log('Error:', error.message)
   });
-  console.log('mail should have sent')
+  console.log('...mail should have sent')
 }
 
 exports.handler = async (event, context) => {
@@ -30,7 +30,7 @@ exports.handler = async (event, context) => {
   const data = await lookup()
   // const purchasable = data.availabilities[0].shipping.purchasable
   
-  await sendMail('testing netflify')
+  await sendMail('testing netflify & mailgun')
   // console.log('mail sent and returning status 200')
   return {
     statusCode: 200,
